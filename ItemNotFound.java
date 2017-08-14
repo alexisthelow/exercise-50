4ad31168832ad8ef09af189fca232dfedeb1a227
Alexis Low <alexisthelow@gmail.com>
2017-08-14 08:48:58 -0600

    first

---
+++ ItemNotFound.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ItemNotFound {
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		
+		String[] heroes = {
+			"Abderus", "Achilles", "Aeneas", "Ajax", "Amphitryon",
+			"Bellerophon", "Castor", "Chrysippus", "Daedalus", "Diomedes",
+			"Eleusis", "Eunostus", "Ganymede", "Hector", "Iolaus", "Jason",
+			"Meleager", "Odysseus", "Orpheus", "Perseus", "Theseus"
+		};
+		String guess;
+		boolean found;
+		
+		System.out.println("Pop Quiz!");
+		System.out.println("Name any *mortal* hero from Greek mythology: ");
+		System.out.print("> ");
+		guess = keyboard.next();
+		
+		found = false;
+		for(String hero : heroes) {
+			if(guess.equals(hero)) {
+				System.out.println("That's one of them!");
+				found = true;
+			}
+		}
+		
+		if (found == false) {
+			System.out.println("No, " + guess + " wasn't a mortal Greek hero.");
+		}
+	}
+}
